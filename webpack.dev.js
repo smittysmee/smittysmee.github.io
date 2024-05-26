f471c29d58ad4e04ead3968beaeca421efc15149
smitty <alex@butterflat.io>
2024-05-26 15:13:49 -0500

    - Removed maps key

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,8 +26,6 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-                // MAPS_API_KEY: '',
-                MAPS_API_KEY: 'AIzaSyDAec8TSIv-nNMDqoWCJMY8IVtxuN_gHP8',
 ***REMOVED***
 ***REMOVED***
         ***REMOVED***)
